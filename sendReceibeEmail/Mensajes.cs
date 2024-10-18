83a71da513e894e4cf7cc16741da72583c697d4a
Izarbe02 <zarbix02@gmail.com>
2024-10-18 12:24:02 +0200

    Cambios

---
+++ sendReceibeEmail/Mensajes.cs
@@ -0,0 +1,13 @@
+namespace SendEmail;
+
+
+
+public class Mensajes{
+
+ public string  Autor { get; set; }
+ public string Asunto {get; set;}
+
+public string Cuerpo {get; set;}
+
+public string Fecha {get; set;}
+}
