a725aae261761f00b5f25935c345034a709217f5
Izarbe02 <zarbix02@gmail.com>
2024-11-08 13:40:33 +0100

    YA ni se

--- NewApi/Controllers/AsientoController.cs
+++ NewApi/Controllers/AsientoController.cs
@@ -1,3 +1,71 @@
-namespace Models;
 
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace RestauranteAPI.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class Asientos : ControllerBase
+   {
+       private static List<Asientos> Asiento = new List<Asientos>();
+
+       [HttpGet]
+       public ActionResult<IEnumerable<Asientos> GetAsientos()
+       {
+           return Ok(entradas);
+       }
+
+       [HttpGet("{id}")]
+       public ActionResult<Entradas>GetEntradas(int id)
+       {
+                var entrada = entradas.FirstOrDefault(p => p.Id == id);
+        if (entrada == null)
+                {
+                    return NotFound();
+                }
+                return Ok(entrada);
+       }
+
+       [HttpPost]
+
+       public ActionResult<Entradas> CreateEntrada(Entradas entrada)
+       entrada.Add(entrada);
+       return CreatedAtAction(nameof(GetEntradas), new { id = entrada.Id }, entrada);
+   
+        [HttpPut("{id}")]
+       public IActionResult UpdateEntrada(int id, Entradas updatedEntrada)
+       {
+           var entrada = entradas.FirstOrDefault(p => p.Id == id);
+           if (entrada == null)
+           {
+               return NotFound();
+           }
+           entrada.Nombre = updatedEntrada.Fila;
+           entrada.Columna = updatedEntrada.Columna;
+           entrada.Id = updatedEntrada.Id;
+            entrada.Ocupado = updatedEntrada.Ocupado
+           return NoContent();
+       }
+
+
+
+       [HttpDelete("{id}")]
+       public IActionResult DeleteEntrada(int id)
+       {
+           var entrada = entradas.FirstOrDefault(p => p.Id == id);
+           if (entrada == null)
+           {
+               return NotFound();
+           }
+           entradas.Remove(entrada);
+           return NoContent();
+       }
+
+       public static void InicializarDatos()
+       {    
+        entradas.Add(new Entradas());
+       }
+   }
+}
 
